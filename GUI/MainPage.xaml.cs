4e1ce0cebeeaab74ba869190e1436e5584898779
crazyrussian123456 <122416613+crazyrussian123456@users.noreply.github.com>
2023-02-28 10:55:54 -0700

    Added basic functionality to the cells

--- GUI/MainPage.xaml.cs
+++ GUI/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection.Emit;
 using SS;
 using Label = Microsoft.Maui.Controls.Label;
@@ -6,13 +8,13 @@ namespace GUI;
 
 public partial class MainPage : ContentPage
 {
-    private static int colCount = 25;
-    private static int rowCount = 25;
+    private static int colCount = 10;
+    private static int rowCount = 10;
 
     char[] columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
     int[] rows = Enumerable.Range(1, colCount + 1).ToArray();
 
-    private Dictionary<string, string> cells = new();
+    private Dictionary<string, Entry> cells = new();
 
     private AbstractSpreadsheet spreadsheet = new Spreadsheet();
 
@@ -79,6 +81,15 @@ public partial class MainPage : ContentPage
             for (int i = 0; i < colCount - 1; i++)
             {
                 string label = "" + columns[i] + rows[j];
+                Entry entry = new Entry
+                {
+                    StyleId = label,
+                    PlaceholderColor = Colors.Pink,
+                    Text = "",
+                    BackgroundColor = Colors.Pink,
+                    HorizontalTextAlignment = TextAlignment.Center
+                };
+                entry.Focused += OnFocus;
                 stack.Add(
                 new Border
                 {
@@ -87,17 +98,39 @@ public partial class MainPage : ContentPage
                     HeightRequest = 30,
                     WidthRequest = 75,
                     HorizontalOptions = LayoutOptions.Center,
-                    Content =
-                    new Label
-                    {
-                        Text = $"{label}",
-                        BackgroundColor = Color.FromRgb(250, 175, 200),
-                        HorizontalTextAlignment = TextAlignment.Center
-                    }
+                    Content = entry
                 }
-                );
+                ) ;
+                cells.Add(label, entry);
             }
             Grid.Children.Add(stack);
         }
+        Contents.Completed += OnEntryCompleted;
+        ///Added
+        cells[CellName.Text].Completed += OnEntryCompleted;
+    }
+    private void SetCell(object sender, EventArgs e)
+    {
+        cells[CellName.Text].Focus();
+        spreadsheet.SetContentsOfCell(CellName.Text, cells[CellName.Text].Text);
+        cells[CellName.Text].Text = "" + spreadsheet.GetCellValue(CellName.Text);
+    }
+
+    private void OnFocus(object sender, EventArgs e)
+    {
+        Entry entry = (Entry)sender;
+        string name = entry.StyleId;
+        CellName.Text = name;
+        Contents.Text = spreadsheet.GetCellContents(name) + "";
+        Value.Text = spreadsheet.GetCellValue(name) + "";
+        ///Added
+        entry.Text = spreadsheet.GetCellValue(name) + "";
+    }
+
+    private void OnEntryCompleted(object sender, EventArgs e)
+    {
+        Entry entry = (Entry)sender;
+        spreadsheet.SetContentsOfCell(CellName.Text, entry.Text);
+        cells[CellName.Text] = entry;
     }
 }
